a04080d0c97e1cc03fb18b117c69457bceb972ae
gagan kumar <gagan.kumar@scanova.io>
2021-05-19 08:21:48 +0530

    using variables in Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+let countryName = "India";
+let continentName = "Asia";
+let populationCount = 1.38;
+
+console.log(
+    "Country Name is :" + countryName + ",",
+    "Continent Name is :" + continentName + ",",
+    "Total Population is :" + populationCount + " Bln"
+);
